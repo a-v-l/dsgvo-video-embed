a3b897242ae50beac9d6eaea048423ac81334fa9
Philli <phillippamorland@gmail.com>
2020-09-23 12:20:46 +0200

    typo

--- js/dsgvo-video-embed.js
+++ js/dsgvo-video-embed.js
@@ -13,11 +13,11 @@
              '</p>' + '<p>' + django.gettext('More information can be found in the privacy policy of YouTube under: ') +
              '<a href="https://www.google.de/intl/de/policies/privacy/" rel="noopener" target="_blank">' + 'https://policies.google.com/privacy/' +
              '</a>' + '</p>' + '</div>' + '<a class="video-link" href="https://youtu.be/%id%" rel="noopener" target="_blank">' + django.gettext('Link to Video: ') + 'https://youtu.be/%id%' + '</a>' + '<button>' + django.gettext('Play video') + '</button>',
-        vimeo: '<strong>' + django.gettext('External Video') + '</strong>' +
-             '<div>' + '<p>' + django.gettext('Here you can find a video from Vimeo. To protect your data the content will only be loaded after you give your consent. Only then will Vimeo set Cookies that collect information on user behaviour.') +
-             '</p>' + '<p>' + django.gettext('More information can be found in the privacy policy of Vimeo under: ') +
+         vimeo: '<strong>' + django.gettext('external video') + '</strong>' +
+             '<div>' + '<p>' + django.gettext('here you can find a video from vimeo. to protect your data the content will only be loaded after you give your consent. only then will vimeo set cookies that collect information on user behaviour.') +
+             '</p>' + '<p>' + django.gettext('more information can be found in the privacy policy of vimeo under: ') +
              '<a href="https://vimeo.com/privacy" rel="noopener" target="_blank">' + 'https://vimeo.com/privacy' +
-             '</a>' + '</p>' + '</div>' + '<a class="video-link" href="https://vimeo.com/%id%" rel="noopener" target="_blank">' + django.gettext('Link to Video: )' + 'https://vimeo.com/%id%' + '</a>' + '<button>' + django.gettext('Play video') + '</button>'
+             '</a>' + '</p>' + '</div>' + '<a class="video-link" href="https://vimeo.com/%id%" rel="noopener" target="_blank">' + django.gettext('link to video: ') + 'https://vimeo.com/%id%' + '</a>' + '<button>' + django.gettext('play video') + '</button>'
     };
     window.video_iframes = [];
     document.addEventListener("DOMContentLoaded", function () {
